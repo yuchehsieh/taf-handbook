9862080a249b07ed84567c3fd64468bb83034827
Murphy Hsieh <murphyhsieh@leandev.io>
2020-08-07 20:21:55 +0800

    change: add the login_video at the top of login process

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import img_dashboard from './images/dashbaord.png';
 import img_inbox from './images/inbox.png';
 
 import login from './images/login.png';
+import video_login from './images/video_login.mp4';
 
 
 class App extends Component {
@@ -34,7 +35,14 @@ class App extends Component {
 
                 <body className="App-body">
 
-                <Element className="Chapter" name="login-progress">
+                <Element className="Chapter" name="login-progress" style={{flexDirection: 'column'}}>
+
+                    <video autoPlay loop muted style={{width: '-webkit-fill-available', alignSelf: 'flex-start'}}>
+                        <source src={video_login} type="video/mp4"/>
+                    </video>
+
+                    <div style={{flexDirection: 'row', display: 'flex', marginTop: '2em'}}>
+
                     <div
                         className="Chapter-image"
                         style={{
@@ -51,80 +59,80 @@ class App extends Component {
                         </div>
 
                         <ul className="first-layer">
-                            {/*<li>*/}
-                            {/*    ＊使用者在瀏覽器輸入貴公司專屬的好聘網址，即可前往登入畫面，<br/><span className="Text-red">如圖一</span>。建議瀏覽器：Chrome、Firefox、Edge、Safari。<br/>若為*/}
-                            {/*    <span className="Text-bold">首次登入，請參考</span>開通郵件所提供的網址。*/}
-                            {/*</li>*/}
+                            <li>
+                                ＊使用者在瀏覽器輸入貴公司專屬的好聘網址，即可前往登入畫面，<br/><span className="Text-red">如圖一</span>。建議瀏覽器：Chrome、Firefox、Edge、Safari。<br/>若為
+                                <span className="Text-bold">首次登入，請參考</span>開通郵件所提供的網址。
+                            </li>
+
+                            <li>
+                                ＊輸入用戶名稱。若為<span className="Text-bold">首次登入，請參考</span>開通郵件所提供的用戶名稱。
+                            </li>
+
+                            <li>
+                                ＊輸入用戶密碼。若為<span className="Text-bold">首次登入，請參考</span>開通郵件所提供的用戶密碼。
+                            </li>
+
+                            <li>
+                                保持登錄狀態：使用者『勾選』保持登錄狀態，當再度嘗試登入，不需輸入用戶名稱與密碼，即可自動登入。
+                            </li>
+
+                            <li>
+                                ＊按下『登入』鍵，系統即進入首頁畫面。<span className="Text-red">如圖二</span>。
+                            </li>
+
+                            <li>
+                                忘記密碼。請參照<span className="Text-underline">忘記密碼</span>專篇。
+                            </li>
 
                             {/*<li>*/}
-                            {/*    ＊輸入用戶名稱。若為<span className="Text-bold">首次登入，請參考</span>開通郵件所提供的用戶名稱。*/}
+                            {/*    收藏使用者的目前所有的待辦事項<span style={{color: 'red'}}>符</span>系統會自動偵測代辦事項是否完成<span style={{color: 'red'}}>號</span>完成後即自動消*/}
                             {/*</li>*/}
-
                             {/*<li>*/}
-                            {/*    ＊輸入用戶密碼。若為<span className="Text-bold">首次登入，請參考</span>開通郵件所提供的用戶密碼。*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
                             {/*</li>*/}
-
                             {/*<li>*/}
-                            {/*    保持登錄狀態：使用者『勾選』保持登錄狀態，當再度嘗試登入，不需輸入用戶名稱與密碼，即可自動登入。*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
                             {/*</li>*/}
-
                             {/*<li>*/}
-                            {/*    ＊按下『登入』鍵，系統即進入首頁畫面。<span className="Text-red">如圖二</span>。*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
                             {/*</li>*/}
-
                             {/*<li>*/}
-                            {/*    忘記密碼。請參照<span className="Text-underline">忘記密碼</span>專篇。*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
                             {/*</li>*/}
-
-                            <li>
-                                收藏使用者的目前所有的待辦事項<span style={{color: 'red'}}>符</span>系統會自動偵測代辦事項是否完成<span style={{color: 'red'}}>號</span>完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-                            <li>
-                                收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消
-                            </li>
-
-
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
+                            {/*<li>*/}
+                            {/*    收藏使用者的目前所有的待辦事項，系統會自動偵測代辦事項是否完成，完成後即自動消*/}
+                            {/*</li>*/}
 
                         </ul>
+                    </div>
+
                     </div>
                 </Element>
 
@@ -208,7 +216,6 @@ class App extends Component {
                         </ul>
                     </div>
                 </Element>
-
 
                 </body>
             </div>
